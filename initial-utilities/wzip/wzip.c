09474e9bcd72f7378ff319217244c114620d5fde
dzorrilla <dzorrilla@fortinet.com>
2025-06-22 22:43:07 +0200

    [initial-utilities/wzip] : Refactoring, plus adding endianness convention for encoding the char count, and using now 'uintX_t' types to ensure type sizes.

--- initial-utilities/wzip/wzip.c
+++ initial-utilities/wzip/wzip.c
@@ -1,5 +1,22 @@
+#include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+union u16_bytes_u {
+  uint16_t number;
+  uint8_t bytes[sizeof(uint16_t)];
+};
+
+union u32_bytes_u {
+  uint32_t number;
+  uint8_t bytes[sizeof(uint32_t)];
+};
+
+bool is_little_endian(void) {
+  union u16_bytes_u x = {.number = 0x2B1A};
+  return x.bytes[0] == 0x1A;
+}
 
 int main(int argc, char *argv[]) {
   if (argc < 2) {
@@ -7,43 +24,56 @@ int main(int argc, char *argv[]) {
     exit(EXIT_FAILURE);
   }
 
-  char target = 0, c = EOF;
-  unsigned int counter = 0;
-  int files_left = argc - 1, file_idx = 1;
+  int8_t target = 0, c = EOF;
+  union u32_bytes_u count = {.number = 0};
   FILE *fp = NULL;
+  bool little_endian = is_little_endian();
 
-  do {
+  for (int file_idx = 1; file_idx < argc; file_idx++) {
 
-    // Catch a possible EOF: open the next file and get the first character
-    if (c == EOF) {
-      if (fp) // Close the previous file (if any)
-        fclose(fp);
-      fp = fopen(argv[file_idx++], "r");
-      if (!fp) {
-        printf("wzip: cannot open file\n");
-        exit(EXIT_FAILURE);
+    // Open file
+    fp = fopen(argv[file_idx], "r");
+    if (!fp) {
+      printf("wzip: cannot open file\n");
+      exit(EXIT_FAILURE);
+    }
+
+    // Read file
+    while ((c = fgetc(fp)) != EOF) {
+      if (target == c || target == 0) {
+        count.number++;
+      } else {
+
+        // We stick to little endian as the standard way of encoding the count
+        if (!little_endian) {
+          // Swap bytes 3 and 0
+          count.bytes[0] = count.bytes[3] ^ count.bytes[0]; // B3 xor B0
+          count.bytes[3] = count.bytes[3] ^ count.bytes[0]; // B0
+          count.bytes[0] = count.bytes[3] ^ count.bytes[0]; // B3
+          // Swap bytes 2 and 1
+          count.bytes[1] = count.bytes[2] ^ count.bytes[1]; // B2 xor B1
+          count.bytes[2] = count.bytes[2] ^ count.bytes[1]; // B1
+          count.bytes[1] = count.bytes[2] ^ count.bytes[1]; // B2
+        }
+
+        // flush the count and the char
+        fwrite(&count, sizeof(count), 1,
+               stdout);                             // Write the occurrences
+        fwrite(&target, sizeof(target), 1, stdout); // Write the char
+        count.number = 1;                           // Reset the count
       }
-      files_left--;
-      c = fgetc(fp);
+
+      // Update the target char
+      target = c;
     }
 
-    if (target != c && target != 0) {
-      fwrite(&counter, sizeof(unsigned int), 1,
-             stdout);                           // Write the occurrences
-      fwrite(&target, sizeof(char), 1, stdout); // Write the char
-      counter = 1;                              // Reset the counter
-    } else {
-      counter++;
-    }
-    target = c;
-  } while ((c = fgetc(fp)) != EOF || files_left > 0);
+    // Close file
+    fclose(fp);
+  }
 
-  // Write the last char encoding
-  fwrite(&counter, sizeof(unsigned int), 1,
-         stdout);                           // Write the occurrences
-  fwrite(&target, sizeof(char), 1, stdout); // Write the char
-
-  fclose(fp); // Close last file opened
+  // flush the last char read
+  fwrite(&count, sizeof(count), 1, stdout);   // Write the occurrences
+  fwrite(&target, sizeof(target), 1, stdout); // Write the char
 
   exit(EXIT_SUCCESS);
 }
